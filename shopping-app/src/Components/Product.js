35c7c7346fba91424c723d841648c0d0fbf16549
jamshaidmehmood021 <jamshaid.mehmood@arbisoft.com>
2024-07-12 16:08:11 +0500

    Using Thunk for async data operation and adding robustness to cart page

--- shopping-app/src/Components/Product.js
+++ shopping-app/src/Components/Product.js
@@ -1,28 +1,42 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import { add } from '../Store/cartSlice';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import {fetchProducts} from "../Store/productSlice"
+import { STATUS } from '../Store/productSlice';
+import { Bars } from 'react-loading-icons'
+
 
 const Product = () => {
     const dispatch = useDispatch()
+    const {data: products, status} = useSelector((state) => state.product)
 
-    const [products, setProducts] = useState([]);
+    //const [products, setProducts] = useState([]);
 
     useEffect(() =>{
-        const fetchData = async () =>{
-            const response = await fetch ("https://fakestoreapi.com/products")
-            const data = await response.json()
-            //console.log(data)
-            setProducts(data)
-        }
-        fetchData()
-    },[])
+        
+        dispatch(fetchProducts());
+        // const fetchData = async () =>{
+        //     const response = await fetch ("https://fakestoreapi.com/products")
+        //     const data = await response.json()
+        //     //console.log(data)
+        //     setProducts(data)
+        // }
+        // fetchData()
+    },[dispatch])
 
     const handleAddToCart = ( product) =>{
         dispatch(add(product))
 
     }
 
-
+    if(status === STATUS.LOADING)
+    {
+        return <Bars stroke="#98ff98" />
+    }
+    if(status === STATUS.ERROR)
+    {
+        return <p>Something went Wrong .....</p>
+    }
 
   return (
     <div className="productsWrapper">
