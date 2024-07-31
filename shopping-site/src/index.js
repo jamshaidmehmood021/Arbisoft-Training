32653ab4945cf2878aaf382e7ae29119126d8447
jamshaidmehmood021 <jamshaid.mehmood@arbisoft.com>
2024-07-31 15:29:33 +0500

    Design Assessment Complete with constraints

--- shopping-site/src/index.js
+++ shopping-site/src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Server } from 'miragejs';
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Server } from 'miragejs';
+
 
 new Server({
   routes() {
