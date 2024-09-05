4b81437a8092692234c301daf3b512420c9b3402
jamshaidmehmood021 <jamshaid.mehmood@arbisoft.com>
2024-09-05 11:04:01 +0500

    backend starter code

---
+++ Backend/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const authRoutes = require('./routes/auth/auth');
+require('./configs/passport');
+const { sequelize } = require('./lib/sequelize');
+const cors = require('cors')
+
+
+const app = express();
+app.use(cors())
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.use('/', authRoutes);
+
+app.listen(PORT, async () => {
+    console.log(`Server started on http://localhost:${PORT}`);
+    try {
+        await sequelize.authenticate();
+        console.log('Database connection has been established successfully.');
+        await sequelize.sync({ alter: true });
+        console.log('Models have been synchronized!');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+});
